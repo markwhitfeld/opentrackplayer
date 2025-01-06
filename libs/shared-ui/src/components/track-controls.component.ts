0f6a483908bd80f33a7046fff6bdc808999da6f5
markwhitfeld <mark.whitfeld@gmail.com>
2025-01-06 10:53:49 +0200

    fix: correct solo prop

--- libs/shared-ui/src/components/track-controls.component.ts
+++ libs/shared-ui/src/components/track-controls.component.ts
@@ -72,12 +72,12 @@ import { CommonModule } from "@angular/common";
       </mat-button-toggle-group>
 
       <button
+        *ngIf="false"
         mat-icon-button
         [color]="track().focused ? 'accent' : ''"
-        (click)="focusToggle.emit()"
+        (click)="soloToggle.emit()"
       >
-        <mat-icon>{{ track().focused ? "headset" : "headset_off" }}</mat-icon>
-        <!--mat-icon>headphones</mat-icon-->
+        <mat-icon>headphones</mat-icon>
       </button>
     </div>
   `,
@@ -110,5 +110,5 @@ export class TrackControlsComponent {
   volumeChange = output<number>();
   panChange = output<number>();
   muteToggle = output<void>();
-  focusToggle = output<void>();
+  soloToggle = output<void>();
 }
